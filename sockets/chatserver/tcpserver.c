75f7f147f12e81d5286173b42f65fb33322884a3
Fernando Tschopp <tschoppfernando@gmail.com>
2017-12-13 00:17:41 -0300

    cambios

--- sockets/chatserver/tcpserver.c
+++ sockets/chatserver/tcpserver.c
@@ -10,24 +10,25 @@
 #include <sys/types.h>
 #include "tcpserver.h"
 
-
 void createTCPServer(tcpServer *server)
 {
-    /* Socket settings */
+	/* Socket settings */
 	server->listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	
+
 	server->addr.sin_family = AF_INET;
 	server->addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	server->addr.sin_port = htons(server->port); 
+	server->addr.sin_port = htons(server->port);
 	printf("\nCreating server...");
-    /* Bind */
-	if(bind(server->listenfd, (struct sockaddr*)&server->addr , sizeof(server->addr)) < 0){
+	/* Bind */
+	if (bind(server->listenfd, (struct sockaddr *)&server->addr, sizeof(server->addr)) < 0)
+	{
 		perror("Socket binding failed");
 	}
 	printf("\nBinding OK");
 
-    /* Listen */
-	if(listen(server->listenfd, server->max_connection) < 0){
+	/* Listen */
+	if (listen(server->listenfd, server->max_connection) < 0)
+	{
 		perror("Socket listening failed");
 	}
 	printf("\nListening on %d", server->port);
@@ -37,10 +38,10 @@ void createTCPServer(tcpServer *server)
 int acceptConnection(int listenfd)
 {
 	struct sockaddr_in addr;
-    socklen_t clilen = sizeof(addr);
-	int connfd = accept(listenfd, (struct sockaddr*)&addr, &clilen); 
-	
-	if(connfd < 0)
+	socklen_t clilen = sizeof(addr);
+	int connfd = accept(listenfd, (struct sockaddr *)&addr, &clilen);
+
+	if (connfd < 0)
 	{
 		perror("accept error");
 		return -1;
@@ -49,43 +50,45 @@ int acceptConnection(int listenfd)
 	return connfd;
 }
 
-//Imprime la IP 
+//Imprime la IP
 void print_ip_addr(struct sockaddr_in addr)
 {
 	printf("\n%d.%d.%d.%d",
-		addr.sin_addr.s_addr & 0xFF,
-		(addr.sin_addr.s_addr & 0xFF00)>>8,
-		(addr.sin_addr.s_addr & 0xFF0000)>>16,
-		(addr.sin_addr.s_addr & 0xFF000000)>>24);
+		   addr.sin_addr.s_addr & 0xFF,
+		   (addr.sin_addr.s_addr & 0xFF00) >> 8,
+		   (addr.sin_addr.s_addr & 0xFF0000) >> 16,
+		   (addr.sin_addr.s_addr & 0xFF000000) >> 24);
 }
 
 //Lee datos del socket
 int readSocket(int sockfd, char *buff_in, int len)
-{	
+{
 	int bytesRead = recv(sockfd, buff_in, len, 0);
 	//printf("\nBytes Leidosss %d", bytesRead);
 
-	if(bytesRead < 0){
+	if (bytesRead < 0)
+	{
 		perror("ReadSocket error");
 	}
 	else
 	{
-		buff_in[bytesRead] = '\0';	
+		buff_in[bytesRead] = '\0';
 	}
 
-	return  bytesRead;
+	return bytesRead;
 }
 
 //Escribe en el socket
 int writeSocket(int sockfd, char *buff_out, int len)
 {
-	int bytesSended = send(sockfd, buff_out, len, 0);	
+	int bytesSended = send(sockfd, buff_out, len, 0);
 
-	if(bytesSended < 0){
+	if (bytesSended < 0)
+	{
 		perror("SendSocket error");
-	}	
+	}
 
-	return  bytesSended;
+	return bytesSended;
 }
 
 //Cierra ambos lados del socket tanto el cliente como el server
@@ -100,37 +103,67 @@ void shutdownSocket(int sockfd)
 	shutdown(sockfd, SHUT_WR);
 }
 
+// int connectTCPServer(char *hostname, int port)
+// {
+// 	int sockfd;
+// 	struct sockaddr_in addr;
+// 	struct hostent *server;
+// 	int connfd;
+
+// 	/* Create a socket point */
+// 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+// 	printf("sockfd %d ",sockfd);
+// 	if (sockfd < 0) {
+// 		perror("ERROR opening socket");
+// 		exit(1);
+// 	}
+
+// 	//server = gethostbyname(hostname);
+
+// 	//if (server == NULL) {
+// 	// 	fprintf(stderr,"ERROR, no such host\n");
+// 	// 	exit(0);
+// 	// }
+
+// 	addr.sin_family = AF_INET;
+// 	addr.sin_port = htons(port);
+
+// 	/* Now connect to the server */
+// 	connfd = connect(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+// 	if (connfd  < 0) {
+// 		perror("ERROR connecting");
+// 		exit(1);
+// 	}
+
+// 	return sockfd;
+// }
 
 int connectTCPServer(char *hostname, int port)
 {
+	struct sockaddr_in serv_addr;
 	int sockfd;
-	struct sockaddr_in addr;
-	struct hostent *server;
-	int connfd;
-   
-	/* Create a socket point */
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-	if (sockfd < 0) {
-		perror("ERROR opening socket");
-		exit(1);
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("\n Error : Could not create socket \n");
+		return 1;
 	}
 
-	server = gethostbyname(hostname);
+	memset(&serv_addr, '0', sizeof(serv_addr));
 
-	if (server == NULL) {
-		fprintf(stderr,"ERROR, no such host\n");
-		exit(0);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+
+	if (inet_pton(AF_INET, hostname, &serv_addr.sin_addr) <= 0)
+	{
+		printf("\n inet_pton error occured\n");
+		return -1;
 	}
 
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-
-	/* Now connect to the server */
-	connfd = connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)); 
-	if (connfd  < 0) {
-		perror("ERROR connecting");
-		exit(1);
+	int connfd = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+	if (connfd < 0)
+	{
+		printf("\n Error : Connect Failed \n");
+		return -1;
 	}
 
 	return sockfd;
